5319e893638c180766d19107e8fd7675756c1d60
Ankita Sharma <60ankitasharma@gmail.com>
2024-06-23 15:55:52 +0530

    Function Scope

---
+++ Ex_19062024/Lab067.py
@@ -0,0 +1,7 @@
+gobal_a = 10
+
+def f1():
+    print(gobal_a)
+    
+
+f1()
---
+++ Ex_19062024/Lab071.py
@@ -0,0 +1,10 @@
+# def sum(a,b):
+#     return a + b
+#
+# add = sum(4,5)
+# print(add)
+
+# lambda is use for one liner
+
+sum = lambda a,b: a+b
+print(sum(4,6))
